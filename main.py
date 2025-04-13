fix: add alpha_t for softmax focal loss

main.py
@@ -126,7 +126,7 @@ def get_args_parser():
     parser.add_argument("--cls_loss_coef", default=2.0, type=float)
     parser.add_argument("--bbox_loss_coef", default=5.0, type=float)
     parser.add_argument("--giou_loss_coef", default=2.0, type=float)
-    parser.add_argument("--focal_alpha", default=0.25, type=float)
+    parser.add_argument("--focal_alpha", default=[0.9, 0.2], type=float)
 
     # dataset parameters
     parser.add_argument("--dataset_file", default="chart")
@@ -203,6 +203,11 @@ def main(args):
     random.seed(seed)
 
     model, criterion, post_processors = build_model(args)
+    # torch.save(
+    #     model.state_dict(),
+    #     r"C:\Users\trand\longg\document\selfstudy\hifuse\reference\trifuse_sam_scratch.pth",
+    # )
+    # return
     model.to(device)
 
     model_without_ddp = model

models/segmentation.py
@@ -221,7 +221,7 @@ def dice_loss(inputs, targets, num_boxes):
 
 
 def softmax_focal_loss(
-    inputs, targets, num_boxes, alpha: float = 0.25, gamma: float = 2
+    inputs, targets, num_boxes, alpha: list = [0, 0.9, 0.2], gamma: float = 2
 ):
     """
     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
@@ -242,8 +242,8 @@ def softmax_focal_loss(
     p_t = torch.exp(-ce_loss)
     loss = ce_loss * ((1 - p_t) ** gamma)
 
-    if alpha >= 0:
-        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+    if alpha:
+        alpha_t = alpha[targets]
         loss = alpha_t * loss
 
     return loss.mean(1).sum() / num_boxes